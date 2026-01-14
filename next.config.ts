0ab75644ab33595cf2652cbfaa67b6012fc0c149
sarthakPabbly <sarthak.tiwari@pabbly.com>
2026-01-14 12:12:05 +0530

    debuggig production bug

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    images: {
+    domains: ['fakestoreapi.com'],
     remotePatterns: [new URL('https://fakestoreapi.com/**')],
   },
    compiler: {
